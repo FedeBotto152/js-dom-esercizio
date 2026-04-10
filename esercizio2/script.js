162e7baa4f48e12def678409fb5a96896a36440f
Federico Botto <federicobottogenova@gmail.com>
2026-04-10 19:26:05 +0200

    fix6

--- esercizio2/script.js
+++ esercizio2/script.js
@@ -1,6 +1,40 @@
+const emailInput = document.querySelector(".email");
+const firstNameInput = document.querySelector(".firstname");
+const lastNameInput = document.querySelector(".lastname");
 const button = document.querySelector(".btn");
-const body = document.body;
+const titleName = document.querySelector(".title-name");
+const printedForm = document.querySelector(".printedform");
+
+const savedData = localStorage.getItem("userForm");
+
+if (savedData) {
+  const user = JSON.parse(savedData);
+
+  emailInput.value = user.email;
+  firstNameInput.value = user.firstName;
+  lastNameInput.value = user.lastName;
+
+  titleName.textContent = user.firstName;
+
+  printedForm.textContent =
+    "Email: " + user.email +
+    " | Nome: " + user.firstName +
+    " | Cognome: " + user.lastName;
+}
 
 button.addEventListener("click", function () {
-  body.classList.toggle("theme");
+  const user = {
+    email: emailInput.value,
+    firstName: firstNameInput.value,
+    lastName: lastNameInput.value
+  };
+
+  localStorage.setItem("userForm", JSON.stringify(user));
+
+  titleName.textContent = user.firstName;
+
+  printedForm.textContent =
+    "Email: " + user.email +
+    " | Nome: " + user.firstName +
+    " | Cognome: " + user.lastName;
 });
