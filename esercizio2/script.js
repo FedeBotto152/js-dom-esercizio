d5137b23a389ae03f01035bddbadad3ceee3300c
Federico Botto <federicobottogenova@gmail.com>
2026-04-10 19:11:27 +0200

    fix3

---
+++ esercizio2/script.js
@@ -0,0 +1,8 @@
+const paragrafi = document.getElementsByClassName("paragrafo");
+console.log("Paragrafi:", paragrafi);
+
+const elemento3 = document.getElementById("elemento-3");
+console.log("Elemento 3:", elemento3);
+
+const titoli = document.getElementsByTagName("h2");
+console.log("Titoli h2:", titoli);
