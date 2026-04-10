0ef4eb1262d2d147239399b9af0061a2b472d095
Federico Botto <federicobottogenova@gmail.com>
2026-04-10 19:23:29 +0200

    fix5

--- esercizio2/script.js
+++ esercizio2/script.js
@@ -1,14 +1,6 @@
-const firstNameInput = document.getElementById("firstName");
-const lastNameInput = document.getElementById("lastName");
-const ageInput = document.getElementById("age");
-const form = document.getElementById("form");
+const button = document.querySelector(".btn");
+const body = document.body;
 
-const person = {
-  firstName: firstNameInput.value,
-  lastName: lastNameInput.value,
-  age: ageInput.value
-};
-
-form.setAttribute("data-person", JSON.stringify(person));
-
-console.log(person);
+button.addEventListener("click", function () {
+  body.classList.toggle("theme");
+});
